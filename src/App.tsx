afa5bc8b507ea9c7a37720a7d8768ac4f33081e1
storch7 <guilhermestorch7@gmail.com>
2025-09-04 12:34:00 -0300

    Retorno de lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
 import { useState } from "react";
 
-
 function TaskForm({ onAddTask }) {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // adicionar funcao pra salvar as infos da tarefa
+    if (!name) return;
     onAddTask(name, description);
-    if(!name) return;
     setName('');
     setDescription('');
-  }
+  };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="text"
-          placeholder="Nome da tarefa" 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input 
-          type="text" 
-          placeholder="Descrição"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <button type="submit">Adicionar Tarefa</button>
-      </form>
-    </>
-  )
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        placeholder="Nome da tarefa" 
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input 
+        type="text" 
+        placeholder="Descrição"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button type="submit">Adicionar Tarefa</button>
+    </form>
+  );
 }
 
 function Task({ task }) {
@@ -40,7 +36,6 @@ function Task({ task }) {
     <div>
       <h3>{task.name}</h3>
       <p>{task.description}</p>
-      <button>Concluir</button>
     </div>
   );
 }
@@ -51,24 +46,23 @@ function App() {
   const handleAddTask = (name, description) => {
     const newTask = {
       id: Date.now(),
-      name: name,
-      description: description,
-      isComplete: false,
+      name,
+      description,
     };
     setTasks(prevTasks => [...prevTasks, newTask]);
-  }
+  };
 
   return (
-    <>
-      <h1>Todolist</h1>
+    <div>
+      <h1>To-Do List</h1>
       <TaskForm onAddTask={handleAddTask}/>
       <div>
         {tasks.map(task => (
           <Task key={task.id} task={task}/>
         ))}
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
