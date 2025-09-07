eb2d77fa1d45ff2ba7276b49e2b70eb6b02560a4
ana clara <abarbosaborges17@gmail.com>
2025-09-07 18:23:27 -0300

    exibe e adiciona tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
 
 
-function TaskForm() {
+function TaskForm({ onAddTask }) {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // adicionar funcao pra salvar as infos da tarefa
+    onAddTask(name, description);
+    if(!name) return;
+    setName('');
+    setDescription('');
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input 
           type="text"
           placeholder="Nome da tarefa" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <input 
           type="text" 
           placeholder="Descrição"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
         <button type="submit">Adicionar Tarefa</button>
       </form>
@@ -18,13 +35,38 @@ function TaskForm() {
   )
 }
 
+function Task({ task }) {
+  return (
+    <div>
+      <h3>{task.name}</h3>
+      <p>{task.description}</p>
+      <button>Concluir</button>
+    </div>
+  );
+}
+
 function App() {
-  
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = (name, description) => {
+    const newTask = {
+      id: Date.now(),
+      name: name,
+      description: description,
+      isComplete: false,
+    };
+    setTasks(prevTasks => [...prevTasks, newTask]);
+  }
 
   return (
     <>
       <h1>Todolist</h1>
-      <TaskForm/>
+      <TaskForm onAddTask={handleAddTask}/>
+      <div>
+        {tasks.map(task => (
+          <Task key={task.id} task={task}/>
+        ))}
+      </div>
     </>
   )
 }
